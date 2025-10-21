36ee40a156f7085ffa9b652fd23915aa9ba4330b
xKevin2409 <Kevin.roemer@heidelberg.com>
2025-10-21 15:23:57 +0200

    Implement solid rectangle drawing function

--- cmdline_drawing/rectangles/solid.go
+++ cmdline_drawing/rectangles/solid.go
@@ -1,8 +1,16 @@
 package rectangles
 
+import "fmt"
+
 // Erwartet zwei Seitenlängen `height` und `width`.
 // Zeichnet ein Rechteck mit diesen Seitenlängen auf der Konsole.
 // Das Rechteck soll komplett mit `#`-Zeichen gefüllt sein.
 func DrawSolidRectangle(height, width int) {
-	// TODO
+	// Zeichne eine Zeile auf die Konsole.
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
 }
