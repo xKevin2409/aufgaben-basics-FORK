58c39554a5459e3d1264c78fdf8b137a1c9ad78e
xKevin2409 <Kevin.roemer@heidelberg.com>
2025-11-12 12:11:14 +0100

    Dreieck geschafft

--- cmdline_drawing/triangles/solid.go
+++ cmdline_drawing/triangles/solid.go
@@ -1,10 +1,24 @@
 package triangles
 
+import "fmt"
+
 // Erwartet eine Seitenlänge `length`.
 // Zeichnet ein gleichschenkliges, rechtwinkliges Dreieck mit diesen Seitenlängen auf der Konsole.
 // Das Dreieck soll komplett mit `#`-Zeichen gefüllt sein.
 // Der rechte Winkel soll links unten liegen und die Seiten sollen
 // vertikal bzw. horizontal verlaufen.
 func DrawSolidTriangle(length int) {
-	// TODO
+	//TODO
+
+	for i := 1; i < length+1; i++ {
+		//rahmen := []string{}
+		rahmen := ""
+		for j := i; j > 0; j-- {
+
+			rahmen += "#"
+
+		}
+		fmt.Println(rahmen)
+	}
+
 }
